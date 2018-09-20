d60f71479232b652ca250e5fb1bd12162a21b3cb
raccoonjs <comsamo84@gmail.com>
2018-09-21 04:13:41 +0900

    로컬용 개발환경 준비. 간략한 api 세팅.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+const indexRouter = require('./routes/index');
+const shopRouter = require('./routes/shop');
 
-var indexRouter = require('./routes/index');
-var shopRouter = require('./routes/shop');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
-var express = require('express');
-var router = express.Router();
-require('dotenv').config();
+const express = require('express');
+const router = require('express-promise-router')();
+const db = require('../lib/db');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	console.log(process.env);
-  res.send('respond with a resource');
+	return db.query('select * from shoplist')
+		.then((result) => {
+			res.send(result);
+		});
 });
 
 module.exports = router;
